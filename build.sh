ae4479246f5edb952cd29ca3909a326b4889c9e1
Aleksandr Streltsov <jness.pro@gmail.com>
2017-01-31 20:07:20 +0300

    permissions

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
-#cp /Users/ness/Yandex.Disk.localized/backup/ArtMan.bak ArtMan.bak &&
 docker build . -t jnesspro/mssql-server-linux &&
-#rm ArtMan.bak &&
 docker run -p 1433:1433 -e 'SA_PASSWORD=@QAZxsw2' \
   -v /Users/ness/Yandex.Disk.localized/backup/artmandb:/var/opt/mssql/backup \
-  --name artmandb -d jnesspro/mssql-server-linux &&
-sleep 15 &&
-docker exec --interactive artmandb /bin/bash < restore.sh
+  --name artmandb -d jnesspro/mssql-server-linux
+sleep 10 &&
+docker exec -i artmandb /bin/bash < restore.sh
