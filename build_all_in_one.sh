267fcd46dbbbb47b3251ef9002e2082eda79d3eb
Aleksandr Streltsov <jness.pro@gmail.com>
2017-06-15 23:17:54 +0300

    changes

--- build_all_in_one.sh
+++ build_all_in_one.sh
@@ -4,7 +4,7 @@ docker rmi jnesspro/mssql-server-linux
 docker build . -t jnesspro/mssql-server-linux &&
 docker run -p 1433:1433 -e 'SA_PASSWORD=@QAZxsw2' \
   -v /Users/ness/Yandex.Disk.localized/backup/artmandb:/var/opt/mssql/backup \
-  -v /Users/ness/IdeaProjects/mssql-server-linux-docker/sql:/sql \
+  -v /Users/ness/Projects/docker/mssql-server-linux-docker/sql:/sql \
   --name voxmedb -d jnesspro/mssql-server-linux &&
 sleep 15 &&
 docker exec -i voxmedb /bin/bash < restore_all.sh
